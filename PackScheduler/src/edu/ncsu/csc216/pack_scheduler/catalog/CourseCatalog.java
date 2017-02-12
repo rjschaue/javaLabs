260d40e92549a0333e113a6cb7967f4b1a51509b
Robert Schauer <rjschaue@ncsu.edu>
2017-02-12 16:54:39 -0500

    Another attempt to correct addCourseToCatalog

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalog.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalog.java
@@ -62,7 +62,11 @@ public class CourseCatalog {
 									   String instructorId, String meetingDays, int startTime, int endTime) {
 		Course course;
 		try {
-			course = new Course(name, title, section, credits, instructorId, meetingDays, startTime, endTime);
+			course = new Course(name, title, section, credits, instructorId, meetingDays, startTime, endTime);			
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException(e.getMessage());
+		}
+		try {
 			return catalog.add(course);
 		} catch (IllegalArgumentException e) {
 			return false;
