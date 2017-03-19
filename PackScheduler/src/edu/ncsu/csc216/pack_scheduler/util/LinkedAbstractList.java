154cb3bbbd4630c178b54a68fe941a65076a77c2
Robert Schauer <rjschaue@ncsu.edu>
2017-03-19 17:30:08 -0400

    Implemented LinkedAbstractList and performed JUnit testing

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedAbstractList.java
@@ -0,0 +1,226 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import java.util.AbstractList;
+
+/**
+ * This class is a specific implementation of a linked list
+ * @author Joey Schauer
+ * @param <E> The element to be processed
+ */
+public class LinkedAbstractList<E> extends AbstractList<E> {
+	/** The front node in the list */
+	private ListNode front;
+	/** The size of the list */
+	private int size;
+	/** The capacity of the list */
+	private int capacity;
+	
+	/**
+	 * The constructor for LinkedAbstractList
+	 * @param capacity the capacity of the list
+	 */
+	public LinkedAbstractList(int capacity) {
+		front = null;
+		size = 0;
+		setCapactiy(capacity);
+	}
+	
+	/**
+	 * Sets the capacity of the list
+	 * @param capacity the capacity of the list
+	 * @throws IllegalArgumentException if the capacity is less than 1
+	 */
+	public void setCapactiy(int capacity) {
+		if (capacity > 0) {
+			this.capacity = capacity;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	/**
+	 * Adds a given element to the list at the given index
+	 * @param index is the index for the element to be placed at
+	 * @param element is the element to be added
+	 * @throws IllegalArgumentException if the size equals the capacity or the element is already in the list
+	 * @throws NullPointerException if the given element is null
+	 * @throws IndexOutOfBoundsException if the index is less than 0 or greater than the size
+	 */
+	@Override
+	public void add(int index, E element) {
+		if (size == capacity) {
+			throw new IllegalArgumentException();
+		}
+		
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		
+		for (ListNode p = front; p != null; p = p.next) {
+			if (p.data.equals(element)) {
+				throw new IllegalArgumentException();
+			}
+		}
+		
+		if (index < 0 || index > size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		ListNode current = front;
+		ListNode previous = null;
+		int count = 0;
+		
+		while(current != null && count != index) {
+			previous = current;
+			current = current.next;
+			count++;
+		}
+		
+		if (current == front) {
+			front = new ListNode(element, front);
+		} else {
+			previous.next = new ListNode(element, current);
+		}
+		size++;		
+	}
+	
+	/**
+	 * Removes the element at the given index from the list
+	 * @param index is the index of the element to be removed
+	 * @throws IndexOutOfBoundsException if the index is less than 0 or greater than or equal to the size
+	 * @return the element that was removed or null
+	 */
+	@Override
+	public E remove(int index) {		
+		if (index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		ListNode current = front;
+		ListNode previous = null;
+		int count = 0;
+		
+		while (current != null && count != index) {
+			previous = current;
+			current = current.next;
+			count++;
+		}
+		
+		if (current != null) {
+			if (current == front) {
+				front = front.next;
+			} else {
+				previous.next = current.next;
+			}
+			size--;
+			return current.data;
+		}
+		return null;
+	}
+	
+	/**
+	 * Sets the index in the list to the given element
+	 * @param index is the index to be set
+	 * @param element is the element to be set
+	 * @throws NullPointerException if the element is null
+	 * @throws IllegalArgumentException if the element is already in the list
+	 * @throws IndexOutOfBoundsException if the index is less than 0 or greater than or equal to the size
+	 * @return the element that was replaced
+	 */
+	@Override
+	public E set(int index, E element) {
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		
+		for (ListNode p = front; p != null; p = p.next) {
+			if (p.data.equals(element)) {
+				throw new IllegalArgumentException();
+			}
+		}
+		
+		if (index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		ListNode current = front;
+		ListNode previous = null;
+		int count = 0;
+		
+		while (current != null && count != index) {
+			previous = current;
+			current = current.next;
+			count++;
+		}
+		
+		if (current == null) {
+			return null;
+		}
+		return current.data;
+	}
+	
+	/**
+	 * Gets the element at the given index
+	 * @param index the index of the element to be gotten
+	 * @throws IndexOutOfBoundsException if the index is less than 0 or greater than or equal to size
+	 * @return the element at the given index
+	 */
+	@Override
+	public E get(int index) {
+		if (index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		ListNode current = front;
+		ListNode previous = null;
+		int count = 0;
+		
+		while (current != null && count != index) {
+			previous = current;
+			current = current.next;
+			count++;
+		}
+		if (current == null) {
+			return null;
+		}
+		return current.data;
+	}
+
+	/**
+	 * Returns the size of the list
+	 * @return the size of the list
+	 */
+	@Override
+	public int size() {
+		return size;
+	}
+
+	/**
+	 * This class is used for each node in the list
+	 * @author Joey Schauer
+	 */
+	private class ListNode {
+		/** The data in the node */
+		private E data;
+		/** The next node in the list */
+		private ListNode next;
+		
+		/**
+		 * The constructor for ListNode with no next
+		 * @param data the data for the node
+		 */
+		public ListNode(E data) {
+			this(data, null);
+		}
+		
+		/**
+		 * The main constructor for ListNode
+		 * @param data the data for the node
+		 * @param next the next node in the list
+		 */
+		public ListNode(E data, ListNode next) {
+			this.data = data;
+			this.next = next;
+		}
+	}
+}
---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/LinkedAbstractListTest.java
@@ -0,0 +1,208 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Joey
+ *
+ */
+public class LinkedAbstractListTest {
+
+	/**
+	 * Method to test add
+	 */
+	@Test
+	public void testAdd() {
+		LinkedAbstractList<String> list = new LinkedAbstractList<String>(10);		
+		assertTrue(list.size() == 0);
+		
+		//Add a string to an empty list
+		list.add(0, "String0");
+		assertTrue(list.size() == 1);
+		
+		//Add a string to the end of a list
+		list.add(1, "String1");
+		assertTrue(list.size() == 2);
+		
+		//Add a null string to a list
+		try {
+			list.add(2, null);
+			fail();
+		} catch (NullPointerException e) {
+			assertTrue(list.size() == 2);
+		}
+		
+		//Add a duplicate string to a list
+		try {
+			list.add(2, "String1");
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(list.size() == 2);
+		}
+		
+		//Add a string outside of the bounds of the list (upper)
+		try {
+			list.add(10, "String3");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 2);
+		}
+		
+		//Add a string outside of the bounds of the list (lower)
+		try {
+			list.add(-1, "String3");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 2);
+		}
+		
+		//Add strings past the list capacity
+		try {
+			for (int i = 2; i < 11; i++) {
+				list.add(i, "String" + i);
+				assertTrue(list.size() == (i + 1));
+			}
+		} catch (IllegalArgumentException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+	}
+	
+	/**
+	 * Method to test remove
+	 */
+	@Test
+	public void testRemove() {
+		LinkedAbstractList<String> list = new LinkedAbstractList<String>(10);		
+		assertTrue(list.size() == 0);
+		
+		for (int i = 0; i < 10; i++) {
+			list.add(i, "String" + i);
+			assertTrue(list.size() == (i + 1));
+		}
+		
+		//Remove a string outside of the bounds of the list (lower)
+		try {
+			list.remove(-1);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Remove a string outside of the bounds of the list (upper)
+		try {
+			list.remove(10);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Remove a string from the beginning of the list
+		assertEquals(list.remove(0), "String0");
+		assertTrue(list.size() == 9);
+		
+		//Remove a string from the end of the list
+		assertEquals(list.remove(8), "String9");
+		assertTrue(list.size() == 8);
+		
+		//Remove a string from the middle of the list
+		assertEquals(list.remove(4), "String5");
+		assertTrue(list.size() == 7);
+	}
+	
+	/**
+	 * Method to test set
+	 */
+	@Test
+	public void testSet() {
+		LinkedAbstractList<String> list = new LinkedAbstractList<String>(10);		
+		assertTrue(list.size() == 0);
+		
+		for (int i = 0; i < 10; i++) {
+			list.add(i, "String" + i);
+			assertTrue(list.size() == (i + 1));
+		}
+		
+		//Set a null string to a list
+		try {
+			list.set(0, null);
+			fail();
+		} catch (NullPointerException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Set a duplicate string to a list
+		try {
+			list.set(1, "String1");
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Set a string outside of the bounds of the list (upper)
+		try {
+			list.set(11, "String11");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Set a string outside of the bounds of the list (lower)
+		try {
+			list.set(-1, "String-1");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Set a string at the front of the list
+		assertEquals(list.set(0, "newString0"), "String0");
+		assertTrue(list.size() == 10);
+		
+		//Set a string at the end of the list
+		assertEquals(list.set(9, "newString9"), "String9");
+		assertTrue(list.size() == 10);
+		
+		//Set a string at the end of the list
+		assertEquals(list.set(5, "newString5"), "String5");
+		assertTrue(list.size() == 10);
+	}
+	
+	/**
+	 * Method to test get
+	 */
+	@Test
+	public void testGet() {
+		LinkedAbstractList<String> list = new LinkedAbstractList<String>(10);		
+		assertTrue(list.size() == 0);
+		
+		for (int i = 0; i < 10; i++) {
+			list.add(i, "String" + i);
+			assertTrue(list.size() == (i + 1));
+		}
+		
+		//Get a string outside of the bounds of the list (upper)
+		try {
+			list.get(11);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Get a string outside of the bounds of the list (lower)
+		try {
+			list.get(-1);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Get a string from the list
+		assertEquals(list.get(0), "String0");
+	}
+}
