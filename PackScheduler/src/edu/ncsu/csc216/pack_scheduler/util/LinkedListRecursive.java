96bc11eb148e068375fa571028bf31005fed4d93
Robert Schauer <rjschaue@ncsu.edu>
2017-04-16 13:20:58 -0400

    Implemented LinkedListRecursive and tested

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursive.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursive.java
@@ -4,26 +4,45 @@
 package edu.ncsu.csc216.pack_scheduler.util;
 
 /**
+ * This is a linked list using recursion for it's implementation
  * @author Joey Schauer
- * @param <E>
+ * @param <E> Generic type for the linked list
  */
 public class LinkedListRecursive<E> {
+	/** Gets the front list node for the linked list */
 	private ListNode front;
+	/** Gets the size of the list */
 	private int size;
 	
+	/**
+	 * Null constructor for LinkedListRecursive
+	 */
 	public LinkedListRecursive() {
 		front = null;
 		size = 0;
 	}
 	
+	/**
+	 * Returns the size of the linked list
+	 * @return the size of the linked list
+	 */
 	public int size() {
 		return size;
 	}
 	
+	/**
+	 * Returns true if the list is empty
+	 * @return true if the list is empty
+	 */
 	public boolean isEmpty() {
 		return size == 0;
 	}
 
+	/**
+	 * Returns true if the linked list contains a given element
+	 * @param element the element to check the list for
+	 * @return true if the linked list contains a given element
+	 */
 	public boolean contains(E element) {
 		if (front == null) {
 			return false;
@@ -31,6 +50,12 @@ public class LinkedListRecursive<E> {
 		return front.contains(element);
 	}
 	
+	/**
+	 * Adds a given element to the end of the linked list
+	 * @param element the element to be added to the list
+	 * @return true if the element is successfully added to the list
+	 * @throws IllegalArgumentException if the element is already in the list
+	 */
 	public boolean add(E element) {
 		if (contains(element)) {
 			throw new IllegalArgumentException();
@@ -51,6 +76,15 @@ public class LinkedListRecursive<E> {
 		}
 	}
 	
+	/**
+	 * Adds a given element at the given index in the linked list
+	 * @param idx the index to add the given element
+	 * @param element the element to be added
+	 * @return true if the element was successfully added at the given index
+	 * @throws NullPointerException if the given element is null
+	 * @throws IllegalArgumentException if the element is already in the list
+	 * @throws IndexOutOfBoundsException if the index is less than 0 or greater than size
+	 */
 	public boolean add(int idx, E element) {
 		if (element == null) {
 			throw new NullPointerException();
@@ -69,59 +103,194 @@ public class LinkedListRecursive<E> {
 		return front.add(idx, element);
 	}
 	
+	/**
+	 * Returns the element at the given index
+	 * @param idx the index to get the element from
+	 * @return the element at the given index
+	 * @throws IndexOutOfBoundsException if the index is less than 0 or greater than or equal to size
+	 */
 	public E get(int idx) {
-		return null;
+		if (idx < 0 || idx >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		return front.get(idx);
 	}
 	
+	/**
+	 * Removes the node at the given index in the linked list
+	 * @param idx the index to remove from the list
+	 * @return the element that was removed from the list
+	 * @throws IndexOutOfBoundsException if the index is less than 0 or greater than or equal to size
+	 */
 	public E remove(int idx) {
-		return null;
+		if (idx < 0 || idx >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (idx == 0) {
+			E removed = front.data;
+			front = front.next;
+			size--;
+			return removed;
+		}
+		return front.remove(idx);
 	}
 	
+	/**
+	 * Removes the given element from the linked list
+	 * @param element the element to be removed from the linked list
+	 * @return true if the element was successfully removed from the list
+	 * @throws NullPointerException if the given element is null
+	 * @throws IllegalArgumentException if the list is empty
+	 */
 	public boolean remove(E element) {
-		return false;
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		if (isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		if (!contains(element)) {
+			return false;
+		}
+		if (front.data.equals(element)) {
+			front = front.next;
+			size--;
+			return true;
+		}
+		return front.remove(element);
 	}
 	
+	/**
+	 * Sets the element at the given index to the given element
+	 * @param idx the index to set with the given element
+	 * @param element the element to set
+	 * @return the element that was replaced
+	 * @throws NullPointerException if the given element is null
+	 * @throws IllegalArgumentException if the element is already in the list
+	 * @throws IndexOutOfBoundsException if the index is less than 0 or greater than or equal to size
+	 */
 	public E set(int idx, E element) {
-		return null;
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		if (contains(element)) {
+			throw new IllegalArgumentException();
+		}
+		if (idx < 0 || idx >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		return front.set(idx, element);
 	}
 	
+	/**
+	 * Nested class that handles list node functionality for the linked list
+	 * @author Joey Schauer
+	 */
 	private class ListNode {
+		/** the data for the list node */
 		public E data;
+		/** the next list node in the linked list */
 		public ListNode next;
 		
+		/**
+		 * The constructor for ListNode
+		 * @param data the data to set the list node to
+		 * @param next the next list node in the linked list
+		 */
 		public ListNode(E data, ListNode next) {
 			this.data = data;
 			this.next = next;
 		}
 		
+		/**
+		 * Searches for a given element using recursion
+		 * @param element the element to check
+		 * @return true if the list node contains the element
+		 */
 		private boolean contains(E element) {
-			if (data == null) {
-				return false;
-			}
 			if (data.equals(element)) {
 				return true;
+			}
+			if (next == null) {
+				return false;
 			}
 			return next.contains(element);
 		}
 		
+		/**
+		 * Adds a given element to the given index using recursion
+		 * @param idx the index to add the element to
+		 * @param element the element to be added
+		 * @return true if the element is successfully added
+		 */
 		private boolean add(int idx, E element) {
-			return false;
+			if (idx == 1) {
+				ListNode list = new ListNode(element, next);
+				this.next = list;
+				size++;
+				return true;
+			}
+			idx--;
+			return next.add(idx, element);
 		}
 		
+		/**
+		 * Returns the element at the given index using recursion
+		 * @param idx the index to get the element from
+		 * @return the element at the given index
+		 */
 		private E get(int idx) {
-			return null;
+			if (idx == 0) {
+				return this.data;
+			}
+			idx--;
+			return next.get(idx);
 		}
 		
+		/**
+		 * Removes the node at the given index using recursion
+		 * @param idx the index to remove the node from
+		 * @return the element from the removed node
+		 */
 		private E remove(int idx) {
-			return null;
+			if (idx == 1) {
+				E removed = next.data;
+				next = next.next;
+				size--;
+				return removed;
+			}
+			idx--;
+			return next.remove(idx);
 		}
 		
+		/**
+		 * Removes the given element from the linked list using recursion
+		 * @param element the element to be removed
+		 * @return true if the element was successfully removed
+		 */
 		private boolean remove(E element) {
-			return false;
+			if (next.data.equals(element)) {
+				next = next.next;
+				size--;
+				return true;
+			}
+			return next.remove(element);
 		}
 		
+		/**
+		 * Sets the given element at the given index
+		 * @param idx the index to set the element to
+		 * @param element the element to be set
+		 * @return the element that was replaced
+		 */
 		private E set(int idx, E element) {
-			return null;
+			if (idx == 0) {
+				E replaced = data;
+				data = element;
+				return replaced;
+			}
+			idx--;
+			return next.set(idx, element);
 		}
 	}
 }
--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursiveTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursiveTest.java
@@ -8,13 +8,256 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
+ * Class to test LinkedListRecursive
  * @author Joey Schauer
  */
 public class LinkedListRecursiveTest {
 
+	/**
+	 * Method to test add
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdd() {
+		LinkedListRecursive<String> list = new LinkedListRecursive<String>();		
+		assertTrue(list.size() == 0);
+		
+		//Add a string to an empty list
+		list.add(0, "String0");
+		assertTrue(list.size() == 1);
+		
+		//Add a string to the end of a list
+		list.add(1, "String1");
+		assertTrue(list.size() == 2);
+		
+		//Add a null string to a list
+		try {
+			list.add(2, null);
+			fail();
+		} catch (NullPointerException e) {
+			assertTrue(list.size() == 2);
+		}
+		
+		//Add a duplicate string to a list
+		try {
+			list.add(2, "String1");
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(list.size() == 2);
+		}
+		
+		//Add a string outside of the bounds of the list (upper)
+		try {
+			list.add(10, "String3");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 2);
+		}
+		
+		//Add a string outside of the bounds of the list (lower)
+		try {
+			list.add(-1, "String3");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 2);
+		}
+		
+		//Add many strings
+		for (int i = 2; i < 11; i++) {
+			list.add(i, "String" + i);
+			assertTrue(list.size() == (i + 1));
+		}
+		
+		//Add a string in the middle of a list
+		list.add(6, "NewString");
+		assertTrue(list.size() == 12);
+		assertTrue(list.get(5).equals("String5"));
+		assertTrue(list.get(7).equals("String6"));
+		
+		//Add a string already in the list
+		try {
+			list.add("String3");
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(list.size() == 12);
+		}
+
+		//Add a string to the end using the element only add
+		assertTrue(list.add("EString"));
+		assertTrue(list.size() == 13);
+		assertTrue(list.get(12).equals("EString"));
+		
+		LinkedListRecursive<String> list2 = new LinkedListRecursive<String>();		
+		//Add a string to an empty list using the element only add
+		assertTrue(list2.add("NewList"));
+		assertTrue(list2.size() == 1);
+		assertTrue(list2.get(0).equals("NewList"));
+	}
+	
+	/**
+	 * Method to test remove
+	 */
+	@Test
+	public void testRemove() {
+		LinkedListRecursive<String> list = new LinkedListRecursive<String>();	
+		assertTrue(list.size() == 0);
+		
+		for (int i = 0; i < 10; i++) {
+			list.add(i, "String" + i);
+			assertTrue(list.size() == (i + 1));
+		}
+		
+		//Remove a string outside of the bounds of the list (lower)
+		try {
+			list.remove(-1);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Remove a string outside of the bounds of the list (upper)
+		try {
+			list.remove(11);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Remove a string from the beginning of the list
+		assertEquals(list.remove(0), "String0");
+		assertTrue(list.size() == 9);
+		
+		//Remove a string from the end of the list
+		assertEquals(list.remove(8), "String9");
+		assertTrue(list.size() == 8);
+		
+		//Remove a string from the middle of the list
+		assertEquals(list.remove(4), "String5");
+		assertTrue(list.size() == 7);
+		
+		//Element only remove a null string 
+		try {
+			list.remove(null);
+			fail();
+		} catch (NullPointerException e) {
+			assertTrue(list.size() == 7);
+		}
+		
+		LinkedListRecursive<String> list2 = new LinkedListRecursive<String>();	
+		//Element only remove a string from an empty list
+		try {
+			list2.remove("String");
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(list2.size() == 0);
+		}
+		
+		//Remove an element not on the list
+		assertFalse(list.remove("NotInList"));
+		
+		//Remove from front of list
+		assertTrue(list.remove("String1"));
+		assertTrue(list.size() == 6);
+		
+		//Remove from end of list
+		assertTrue(list.remove("String8"));
+		assertTrue(list.size() == 5);
+		
+		//Remove from middle of list
+		assertTrue(list.remove("String4"));
+		assertTrue(list.size() == 4);
+	}
+	
+	/**
+	 * Method to test set
+	 */
+	@Test
+	public void testSet() {
+		LinkedListRecursive<String> list = new LinkedListRecursive<String>();		
+		assertTrue(list.size() == 0);
+		
+		for (int i = 0; i < 10; i++) {
+			list.add(i, "String" + i);
+			assertTrue(list.size() == (i + 1));
+		}
+		
+		//Set a null string to a list
+		try {
+			list.set(0, null);
+			fail();
+		} catch (NullPointerException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Set a duplicate string to a list
+		try {
+			list.set(1, "String1");
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Set a string outside of the bounds of the list (upper)
+		try {
+			list.set(11, "String11");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Set a string outside of the bounds of the list (lower)
+		try {
+			list.set(-1, "String-1");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Set a string at the front of the list
+		assertEquals(list.set(0, "newString0"), "String0");
+		assertTrue(list.size() == 10);
+		
+		//Set a string at the end of the list
+		assertEquals(list.set(9, "newString9"), "String9");
+		assertTrue(list.size() == 10);
+		
+		//Set a string in the middle of the list
+		assertEquals(list.set(5, "newString5"), "String5");
+		assertTrue(list.size() == 10);
+	}
+	
+	/**
+	 * Method to test get
+	 */
+	@Test
+	public void testGet() {
+		LinkedListRecursive<String> list = new LinkedListRecursive<String>();		
+		assertTrue(list.size() == 0);
+		
+		for (int i = 0; i < 10; i++) {
+			list.add(i, "String" + i);
+			assertTrue(list.size() == (i + 1));
+		}
+		
+		//Get a string outside of the bounds of the list (upper)
+		try {
+			list.get(11);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Get a string outside of the bounds of the list (lower)
+		try {
+			list.get(-1);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(list.size() == 10);
+		}
+		
+		//Get strings from the list
+		for (int i = 0; i < 10; i++) {
+			assertEquals(list.get(i), "String" + i);
+		}
 	}
 
 }
